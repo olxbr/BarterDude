8890dce14e59237a414b5c17cc7fd163732c3e04
Gabriel de Quadros Ligneul <gabriel.ligneul@olxbr.com>
2020-02-07 14:38:43 -0300

    Add types to hook base classes

--- barterdude/hooks/__init__.py
+++ barterdude/hooks/__init__.py
@@ -1,23 +1,24 @@
 from abc import ABCMeta, abstractmethod
 from asyncworker import RouteTypes
+from barterdude import BarterDude
 
 
 class BaseHook(metaclass=ABCMeta):
     @abstractmethod
-    async def on_success(self, message):
+    async def on_success(self, message: dict):
         '''Called after successfuly consumed the message'''
 
     @abstractmethod
-    async def on_fail(self, message, error):
+    async def on_fail(self, message: dict, error: Exception):
         '''Called when fails to consume the message'''
 
     @abstractmethod
-    async def before_consume(self, message):
+    async def before_consume(self, message: dict):
         '''Called before consuming the message'''
 
 
 class HttpHook(BaseHook):
-    def __init__(self, baterdude, path: str):
+    def __init__(self, baterdude: BarterDude, path: str):
         self.__barterdude = baterdude
         self.__path = path
 
