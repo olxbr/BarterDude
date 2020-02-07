a034f3e3d449553fe7d9ff4df0dedf9a4f5feb99
Gabriel de Quadros Ligneul <gabriel.ligneul@olxbr.com>
2020-02-07 15:11:14 -0300

    Receive logger instead of using root logger

--- barterdude/hooks/logging.py
+++ barterdude/hooks/logging.py
@@ -3,11 +3,18 @@ import logging
 
 
 class Logging(BaseHook):
+    def __init__(
+            self,
+            logger: logging.Logger = logging.getLogger("barterdude")):
+        self.__logger = logger
+
     async def before_consume(self, message):
-        logging.info(f"going to consume message: {message}")
+        self.__logger.info(f"going to consume message: {message}")
 
     async def on_success(self, message):
-        logging.info(f"successfully consumed message: {message}")
+        self.__logger.info(f"successfully consumed message: {message}")
 
     async def on_fail(self, message, error):
-        logging.error(f"failed to consume message ({repr(error)}): {message}")
+        self.__logger.error(
+            f"failed to consume message ({repr(error)}): {message}"
+        )
--- tests/test_hooks/test_logging.py
+++ tests/test_hooks/test_logging.py
@@ -1,4 +1,4 @@
-from asynctest import TestCase, patch
+from asynctest import TestCase, Mock
 from barterdude.hooks.logging import Logging
 
 
@@ -9,27 +9,27 @@ class TestLogging(TestCase):
             "messageTimestamp": "2019-12-26T19:35:10.049Z",
             "chatId": "691645593-18833924-32074499"
         }
-        self.logging = Logging()
+        self.logger = Mock()
+        self.logging = Logging(self.logger)
 
-    @patch("barterdude.hooks.logging.logging.info")
-    async def test_should_log_before_consume(self, mock):
+    async def test_should_log_before_consume(self):
         await self.logging.before_consume(self.message)
-        mock.assert_called_once()
-        mock.assert_called_with(f"going to consume message: {self.message}")
+        self.logger.info.assert_called_once()
+        self.logger.info.assert_called_with(
+            f"going to consume message: {self.message}"
+        )
 
-    @patch("barterdude.hooks.logging.logging.info")
-    async def test_should_log_on_success(self, mock):
+    async def test_should_log_on_success(self):
         await self.logging.on_success(self.message)
-        mock.assert_called_once()
-        mock.assert_called_with(
+        self.logger.info.assert_called_once()
+        self.logger.info.assert_called_with(
             f"successfully consumed message: {self.message}"
         )
 
-    @patch("barterdude.hooks.logging.logging.error")
-    async def test_should_log_on_fail(self, mock):
+    async def test_should_log_on_fail(self):
         await self.logging.on_fail(self.message, KeyError('key not find'))
         error = "KeyError('key not find')"
-        mock.assert_called_once()
-        mock.assert_called_with(
+        self.logger.error.assert_called_once()
+        self.logger.error.assert_called_with(
             f"failed to consume message ({error}): {self.message}"
         )
