3e86f3a7b0c0b62b34178bb3a4f7364e2fea851e
Diogo Munaro Vieira <diogo.munaro@olxbr.com>
2020-04-20 10:12:03 -0300

    Adding more tests for config

---
+++ tests/test_conf.py
@@ -0,0 +1,54 @@
+import logging
+from pythonjsonlogger import jsonlogger
+
+from asynctest import TestCase
+from barterdude.conf import getLogger
+
+
+class TestConf(TestCase):
+
+    def setUp(self):
+        self.log_name = "barterdude"
+        self.log_level = logging.INFO
+
+    async def test_should_get_log_with_default_configs(self):
+        logger = getLogger()
+        self.assertEqual(
+            type(logger.handlers[0]),
+            logging.StreamHandler
+        )
+        self.assertEqual(logger.name, self.log_name)
+        self.assertEqual(logger.level, self.log_level)
+        self.assertEqual(
+            type(logger.handlers[0].formatter),
+            jsonlogger.JsonFormatter
+        )
+
+    async def test_should_get_log_with_custom_configs(self):
+        logger = getLogger("test_log", logging.DEBUG)
+        self.assertEqual(
+            type(logger.handlers[0]),
+            logging.StreamHandler
+        )
+        self.assertEqual(logger.name, "test_log")
+        self.assertEqual(logger.level, logging.DEBUG)
+        self.assertEqual(
+            type(logger.handlers[0].formatter),
+            jsonlogger.JsonFormatter
+        )
+
+    async def test_should_get_log_with_custom_configs_even_called_after(self):
+        logger_first = getLogger()
+        logger = getLogger("test_log", logging.DEBUG)
+        self.assertEqual(
+            type(logger.handlers[0]),
+            logging.StreamHandler
+        )
+        self.assertEqual(logger_first.name, self.log_name)
+        self.assertEqual(logger_first.level, self.log_level)
+        self.assertEqual(logger.name, "test_log")
+        self.assertEqual(logger.level, logging.DEBUG)
+        self.assertEqual(
+            type(logger.handlers[0].formatter),
+            jsonlogger.JsonFormatter
+        )
