a9e9a42a2230f317a80482ef48cf9de92bf9803b
Diogo Munaro Vieira <diogo.munaro@olxbr.com>
2020-05-18 18:37:25 -0300

    Adding redacted control to barterdude logs

--- tests_integration/test_rabbitmq_integration.py
+++ tests_integration/test_rabbitmq_integration.py
@@ -1,13 +1,10 @@
 import os
-import aiohttp
 import asyncio
 
 from asynctest import TestCase
 from barterdude import BarterDude
 from barterdude.monitor import Monitor
-from barterdude.hooks.healthcheck import Healthcheck
 from barterdude.hooks.logging import Logging
-from barterdude.hooks.metrics.prometheus import Prometheus
 from barterdude.message import ValidationException
 from tests_unit.helpers import load_fixture
 from tests_integration.helpers import ErrorHook
@@ -295,133 +292,6 @@ class RabbitMQConsumerTest(TestCase):
             self.assertTrue(message["key"] in first_read)
 
         self.assertSetEqual(second_read, {self.messages[0]["key"]})
-
-    async def test_obtains_healthcheck(self):
-        monitor = Monitor(Healthcheck(self.app))
-
-        @self.app.consume_amqp([self.input_queue], monitor)
-        async def handler(message):
-            pass
-
-        await self.app.startup()
-        await self.queue_manager.put(
-            routing_key=self.input_queue,
-            data=self.messages[0]
-        )
-        await asyncio.sleep(1)
-
-        async with aiohttp.ClientSession() as session:
-            timeout = aiohttp.ClientTimeout(total=1)
-            url = f'http://{self.barterdude_host}:8080/healthcheck'
-            async with session.get(url, timeout=timeout) as response:
-                status_code = response.status
-                text = await response.text()
-
-        self.assertEquals(status_code, 200)
-        self.assertEquals(
-            text,
-            '{"message": "Success rate: 1.0 (expected: 0.95)", '
-            '"fail": 0, "success": 1, "status": "ok"}'
-        )
-
-    async def test_obtains_healthcheck_even_with_crashed_hook(self):
-        monitor = Monitor(ErrorHook(), Healthcheck(self.app))
-
-        @self.app.consume_amqp([self.input_queue], monitor)
-        async def handler(message):
-            pass
-
-        await self.app.startup()
-        await self.queue_manager.put(
-            routing_key=self.input_queue,
-            data=self.messages[0]
-        )
-        await asyncio.sleep(1)
-
-        async with aiohttp.ClientSession() as session:
-            timeout = aiohttp.ClientTimeout(total=1)
-            url = f'http://{self.barterdude_host}:8080/healthcheck'
-            async with session.get(url, timeout=timeout) as response:
-                status_code = response.status
-                text = await response.text()
-
-        self.assertEquals(status_code, 200)
-        self.assertEquals(
-            text,
-            '{"message": "Success rate: 1.0 (expected: 0.95)", '
-            '"fail": 0, "success": 1, "status": "ok"}'
-        )
-
-    async def test_obtains_prometheus_metrics(self):
-        labels = {"app_name": "barterdude_consumer"}
-        monitor = Monitor(Prometheus(self.app, labels))
-
-        @self.app.consume_amqp([self.input_queue], monitor)
-        async def handler(message):
-            pass
-
-        await self.app.startup()
-        await self.queue_manager.put(
-            routing_key=self.input_queue,
-            data=self.messages[0]
-        )
-        await asyncio.sleep(1)
-
-        async with aiohttp.ClientSession() as session:
-            timeout = aiohttp.ClientTimeout(total=1)
-            url = f'http://{self.barterdude_host}:8080/metrics'
-            async with session.get(url, timeout=timeout) as response:
-                status_code = response.status
-                text = await response.text()
-
-        self.assertEquals(status_code, 200)
-        self.assertNotEquals(-1, text.find(
-            'barterdude_received_number_before_consume_messages_total'
-            '{app_name="barterdude_consumer"} 1.0'))
-
-    async def test_register_multiple_prometheus_hooks(self):
-        """This test raised the following error:
-           ValueError: Duplicated timeseries in CollectorRegistry"""
-
-        Monitor(
-            Prometheus(self.app, {"app_name": "barterdude1"}, "/metrics1"),
-            Prometheus(self.app, {"app_name": "barterdude2"}, "/metrics2"),
-        )
-
-    async def test_print_logs(self):
-        monitor = Monitor(Logging())
-        error = Exception("raise expected")
-
-        @self.app.consume_amqp([self.input_queue], monitor)
-        async def handler(message):
-            if message.body == self.messages[0]:
-                raise error
-
-        await self.app.startup()
-
-        with self.assertLogs("barterdude") as cm:
-            await self.queue_manager.put(
-                routing_key=self.input_queue,
-                data=self.messages[0]
-            )
-            await asyncio.sleep(1)
-
-        key = self.messages[0]["key"]
-        error_str = repr(error)
-
-        self.assertIn("'message': 'Before consume message'", cm.output[0])
-        self.assertIn(
-            f"'message_body': '{{\"key\": \"{key}\"}}'", cm.output[0]
-        )
-        self.assertIn("'delivery_tag': 1", cm.output[0])
-
-        self.assertIn("'message': 'Failed to consume message'", cm.output[1])
-        self.assertIn(
-            f"'message_body': '{{\"key\": \"{key}\"}}'", cm.output[1]
-        )
-        self.assertIn("'delivery_tag': 1", cm.output[1])
-        self.assertIn(f"'exception': \"{error_str}\"", cm.output[1])
-        self.assertIn("'traceback': [", cm.output[1])
 
     async def test_fails_to_connect_to_rabbitmq(self):
         monitor = Monitor(Logging())
--- tests_unit/test_hooks/test_logging.py
+++ tests_unit/test_hooks/test_logging.py
@@ -1,6 +1,6 @@
 import logging
 from asynctest import TestCase, Mock, patch
-from barterdude.hooks.logging import Logging
+from barterdude.hooks import logging as hook_log
 from barterdude.conf import BARTERDUDE_DEFAULT_LOG_NAME
 
 
@@ -9,10 +9,10 @@ class TestLogging(TestCase):
 
     def setUp(self):
         self.message = Mock()
-        self.logging = Logging()
+        self.logging = hook_log.Logging()
 
-    async def test_should_access_logger(self):
-        log = Logging("my_log", logging.DEBUG)
+    async def test_should_access_default_logger(self):
+        log = hook_log.Logging("my_log", logging.DEBUG)
         logger = log.logger
         self.assertEqual(
             type(logger),
@@ -22,48 +22,9 @@ class TestLogging(TestCase):
             type(logger.handlers[0]),
             logging.StreamHandler
         )
-        self.assertEqual(logger.name, f"{BARTERDUDE_DEFAULT_LOG_NAME}.my_log")
+        self.assertEqual(
+            logger.name, f"{BARTERDUDE_DEFAULT_LOG_NAME}.my_log")
         self.assertEqual(logger.level, logging.DEBUG)
-
-    @patch("barterdude.hooks.logging.Logging.logger")
-    @patch("barterdude.hooks.logging.json.dumps")
-    async def test_should_log_before_consume(self, dumps, logger):
-        await self.logging.before_consume(self.message)
-        dumps.assert_called_once_with(self.message.body)
-        logger.info.assert_called_once_with({
-            "message": "Before consume message",
-            "delivery_tag": self.message._delivery_tag,
-            "message_body": dumps.return_value,
-        })
-
-    @patch("barterdude.hooks.logging.Logging.logger")
-    @patch("barterdude.hooks.logging.json.dumps")
-    async def test_should_log_on_success(self, dumps, logger):
-        await self.logging.on_success(self.message)
-        dumps.assert_called_once_with(self.message.body)
-        logger.info.assert_called_once_with({
-            "message": "Successfully consumed message",
-            "delivery_tag": self.message._delivery_tag,
-            "message_body": dumps.return_value,
-        })
-
-    @patch("barterdude.hooks.logging.Logging.logger")
-    @patch("barterdude.hooks.logging.json.dumps")
-    @patch("barterdude.hooks.logging.repr")
-    @patch("barterdude.hooks.logging.format_tb")
-    async def test_should_log_on_fail(self, format_tb, repr, dumps, logger):
-        exception = Exception()
-        await self.logging.on_fail(self.message, exception)
-        dumps.assert_called_once_with(self.message.body)
-        repr.assert_called_once_with(exception)
-        format_tb.assert_called_once_with(exception.__traceback__)
-        logger.error.assert_called_once_with({
-            "message": "Failed to consume message",
-            "delivery_tag": self.message._delivery_tag,
-            "message_body": dumps.return_value,
-            "exception": repr.return_value,
-            "traceback": format_tb.return_value,
-        })
 
     @patch("barterdude.hooks.logging.Logging.logger")
     @patch("barterdude.hooks.logging.repr")
@@ -79,6 +40,135 @@ class TestLogging(TestCase):
         logger.error.assert_called_once_with({
             "message": "Failed to connect to the broker",
             "retries": retries,
+            "exception": repr.return_value,
+            "traceback": format_tb.return_value,
+        })
+
+
+class TestLoggingNotRedacted(TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        hook_log.BARTERDUDE_LOG_REDACTED = False
+        self.message = Mock()
+        self.logging = hook_log.Logging()
+
+    @patch("barterdude.hooks.logging.Logging.logger")
+    @patch("barterdude.hooks.logging.json.dumps")
+    async def test_should_log_before_consume(self, dumps, logger):
+        logger.isEnabledFor = Mock(return_value=True)
+        await self.logging.before_consume(self.message)
+        dumps.assert_called_once_with(self.message.body)
+        logger.info.assert_called_once_with({
+            "message": "Before consume message",
+            "delivery_tag": self.message._delivery_tag,
+            "message_body": dumps.return_value,
+        })
+
+    @patch("barterdude.hooks.logging.Logging.logger")
+    @patch("barterdude.hooks.logging.json.dumps")
+    async def test_should_log_before_consume_adding_redact(
+            self, dumps, logger):
+        hook_log.BARTERDUDE_LOG_REDACTED = True
+        self.logging = hook_log.Logging()
+        logger.isEnabledFor = Mock(return_value=True)
+        await self.logging.before_consume(self.message)
+        dumps.assert_not_called()
+        logger.info.assert_called_once_with({
+            "message": "Before consume message",
+            "delivery_tag": self.message._delivery_tag
+        })
+
+    @patch("barterdude.hooks.logging.Logging.logger")
+    @patch("barterdude.hooks.logging.json.dumps")
+    async def test_should_log_on_success(self, dumps, logger):
+        logger.isEnabledFor = Mock(return_value=True)
+        await self.logging.on_success(self.message)
+        dumps.assert_called_once_with(self.message.body)
+        logger.info.assert_called_once_with({
+            "message": "Successfully consumed message",
+            "delivery_tag": self.message._delivery_tag,
+            "message_body": dumps.return_value,
+        })
+
+    @patch("barterdude.hooks.logging.Logging.logger")
+    @patch("barterdude.hooks.logging.json.dumps")
+    @patch("barterdude.hooks.logging.repr")
+    @patch("barterdude.hooks.logging.format_tb")
+    async def test_should_log_on_fail(self, format_tb, repr, dumps, logger):
+        exception = Exception()
+        logger.isEnabledFor = Mock(return_value=True)
+        await self.logging.on_fail(self.message, exception)
+        dumps.assert_called_once_with(self.message.body)
+        repr.assert_called_once_with(exception)
+        format_tb.assert_called_once_with(exception.__traceback__)
+        logger.error.assert_called_once_with({
+            "message": "Failed to consume message",
+            "delivery_tag": self.message._delivery_tag,
+            "message_body": dumps.return_value,
+            "exception": repr.return_value,
+            "traceback": format_tb.return_value,
+        })
+
+
+class TestLoggingRedacted(TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        hook_log.BARTERDUDE_LOG_REDACTED = True
+        self.message = Mock()
+        self.logging = hook_log.Logging()
+
+    @patch("barterdude.hooks.logging.Logging.logger")
+    @patch("barterdude.hooks.logging.json.dumps")
+    async def test_should_log_before_consume(self, dumps, logger):
+        logger.isEnabledFor = Mock(return_value=False)
+        await self.logging.before_consume(self.message)
+        dumps.assert_not_called()
+        logger.info.assert_called_once_with({
+            "message": "Before consume message",
+            "delivery_tag": self.message._delivery_tag
+        })
+
+    @patch("barterdude.hooks.logging.Logging.logger")
+    @patch("barterdude.hooks.logging.json.dumps")
+    async def test_should_log_before_consume_removing_loglevel(
+            self, dumps, logger):
+        hook_log.BARTERDUDE_LOG_REDACTED = False
+        self.logging = hook_log.Logging()
+        logger.isEnabledFor = Mock(return_value=False)
+        await self.logging.before_consume(self.message)
+        dumps.assert_not_called()
+        logger.info.assert_called_once_with({
+            "message": "Before consume message",
+            "delivery_tag": self.message._delivery_tag
+        })
+
+    @patch("barterdude.hooks.logging.Logging.logger")
+    @patch("barterdude.hooks.logging.json.dumps")
+    async def test_should_log_on_success(self, dumps, logger):
+        logger.isEnabledFor = Mock(return_value=False)
+        await self.logging.on_success(self.message)
+        dumps.assert_not_called()
+        logger.info.assert_called_once_with({
+            "message": "Successfully consumed message",
+            "delivery_tag": self.message._delivery_tag
+        })
+
+    @patch("barterdude.hooks.logging.Logging.logger")
+    @patch("barterdude.hooks.logging.json.dumps")
+    @patch("barterdude.hooks.logging.repr")
+    @patch("barterdude.hooks.logging.format_tb")
+    async def test_should_log_on_fail(self, format_tb, repr, dumps, logger):
+        logger.isEnabledFor = Mock(return_value=False)
+        exception = Exception()
+        await self.logging.on_fail(self.message, exception)
+        dumps.assert_not_called()
+        repr.assert_called_once_with(exception)
+        format_tb.assert_called_once_with(exception.__traceback__)
+        logger.error.assert_called_once_with({
+            "message": "Failed to consume message",
+            "delivery_tag": self.message._delivery_tag,
             "exception": repr.return_value,
             "traceback": format_tb.return_value,
         })
