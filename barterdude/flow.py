a0c3b6edb8f22df6965ba80a730ed1bf33924ccf
Diogo Munaro Vieira <diogo.munaro@olxbr.com>
2020-07-31 14:44:02 -0300

    Adding logs to hooks

--- barterdude/flow.py
+++ barterdude/flow.py
@@ -1,4 +1,5 @@
 from barterdude.message import Message
+from barterdude.conf import getLogger
 from barterdude.exceptions import (
     RestartFlowException, StopFailFlowException, StopSuccessFlowException
 )
@@ -7,11 +8,13 @@ from barterdude.exceptions import (
 class Flow:
     def __init__(self, callback):
         self._callback = callback
+        self._logger = getLogger("flow")
 
     async def process(self, message: Message):
         try:
             await self._callback(message)
-        except (StopFailFlowException, StopSuccessFlowException):
-            pass
-        except RestartFlowException:
+        except (StopFailFlowException, StopSuccessFlowException) as e:
+            self._logger.debug(repr(e))
+        except RestartFlowException as e:
+            self._logger.info(repr(e))
             await self.process(message)
--- barterdude/hooks/retry.py
+++ barterdude/hooks/retry.py
@@ -1,5 +1,6 @@
 import asyncio
 from barterdude.hooks import BaseHook
+from barterdude.conf import getLogger
 from barterdude.exceptions import RestartFlowException
 from barterdude.message import Message
 
@@ -11,6 +12,7 @@ class Retry(BaseHook):
             backoff_base_ms: int = 0):
         self._max_tries = max_tries
         self._backoff = backoff_base_ms
+        self._logger = getLogger("retry")
 
     async def on_fail(self, message: Message, error: Exception):
         if not message.properties.headers:
@@ -20,5 +22,7 @@ class Retry(BaseHook):
         message.properties.headers["fail_tries"] = fail_tries
         if self._max_tries == fail_tries:
             return message.reject(False)
-        await asyncio.sleep(self._backoff ** fail_tries / 1000)
-        raise RestartFlowException()
+        backoff = self._backoff ** fail_tries / 1000
+        self._logger.debug(f"Waiting {backoff} seconds for retry...")
+        await asyncio.sleep(backoff)
+        raise RestartFlowException(f"Retrying for the {fail_tries} time")
